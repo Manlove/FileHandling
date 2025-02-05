41f32b5bf882abbe4103379769e3db99630cfa84
Manlove <manlove.logan@gmail.com>
2025-02-05 15:33:35 -0800

    streamlined code

--- split_mergedregs_file.py
+++ split_mergedregs_file.py
@@ -4,25 +4,21 @@
 # a "genes" file with genes and the nearest peaks and outputs a split file with the
 # genes from the mergedregs file on seperate lines. Will duplicate peaks regions
 
-from re import split
-
 # input files
-file_in_path = "/Users/lmanlove/Documents/1_Active_Projects/4930-01SBGWPharma_Integration_45454/Integration/01SBGWPharm_ATAC_mergedregs.txt"
-file_out_path = "/Users/lmanlove/Documents/1_Active_Projects/4930-01SBGWPharma_Integration_45454/Integration/01SBGWPharm_ATAC_split_file.txt"
-gene_file_path = "/Users/lmanlove/Documents/1_Active_Projects/4930-01SBGWPharma_Integration_45454/Integration/01SBGWPharm_ATAC_genes.txt"
+file_in_path = "mergedregs.txt"
+file_out_path = "split_file.txt"
+gene_file_path = "genes.txt"
 
 gene_name_to_symbol = {}
 
 # Steps through the gene file and saves the gene names and matching gene symbols
 with open(gene_file_path, 'r') as gene_file:
-    gene_file.readline()
-    for line in gene_file.readlines():
-
+    next(gene_file)
+    for line in gene_file:
         line = line.strip().split("\t")
-        gene_name_to_symbol[line[1].strip(" ").upper()] = line[0]
+        gene_name_to_symbol[line[1].strip().upper()] = line[0]
 
 
-# Steps thro
 with open(file_in_path, 'r') as file_in:
     with open(file_out_path, 'w') as file_out:
 
@@ -31,19 +27,16 @@ with open(file_in_path, 'r') as file_in:
 
         # Creates a new header from parts of the original header
         # based on the position of the UCSC link column (link_index).
-        # ** Some of this can be simplified**
-        file_out.write("{}\t{}\t".format("Gene Name", "Gene ID"))
-        for i in header[0:link_index-5]:
-            file_out.write("{}\t".format(i))
-        file_out.write("{}".format("Promoter"))
-        file_out.write("\t{}".format(header[link_index-2]))
-        file_out.write("\t{}".format(header[link_index-1]))
-        for i in header[link_index+1:]:
-            file_out.write("\t{}".format(i))
-        file_out.write("\n") # <- remove this and add to start of gene writing to unnecessary line at end
+        file_out.write(
+            "\t".join(
+                ["Gene Name", "Gene ID"] + 
+                header[0:link_index-5] + 
+                ["Promoter", header[link_index-2], header[link_index-1]] + 
+                header[link_index+1:]
+            )
+        )
 
-        
-        for line in file_in.readlines():
+        for line in file_in:
             line = line.strip().split("\t")
 
             # Gene names, distances, and relative positions (upstream, promoter, gene body)
@@ -52,22 +45,25 @@ with open(file_in_path, 'r') as file_in:
             distances = line[link_index - 2].strip('"').split(", ")
             positions = line[link_index - 1].strip('"').split(", ")
 
-            # positions = split(r',(?=")', positions)
             if genes[0] != '': # **fix this, can be simplified**
                 for gene, distance, position in zip(genes, distances, positions):
-                    gene = gene.strip(" ")
+                    gene = gene.strip()
                     if gene.upper() in gene_name_to_symbol:
-                        file_out.write("{}\t{}\t".format(gene, gene_name_to_symbol[gene.upper()]))
-                        for i in line[0:link_index-5]:
-                            file_out.write("{}\t".format(i))
+
                         if int(distance.replace(',','')) >= -7500 and int(distance.replace(',','')) <= 2500:
                             promoter = 1
                         else:
                             promoter = 0
-                        file_out.write("{}\t{}\t{}".format(promoter, distance, position))
-                        for i in line[link_index+1:]:
-                            file_out.write("\t{}".format(i))
-                        file_out.write("\n")
+
+                        outstring = "\t".join(
+                            [gene, gene_name_to_symbol[gene.upper()]] + 
+                            line[0:link_index-5] +
+                            [str(promoter), distance, position] + 
+                            line[link_index+1:]
+                        )
+
+                        file_out.write("\n{}".format(outstring))
+                        
 
 if __name__ == "__main__":
     pass
